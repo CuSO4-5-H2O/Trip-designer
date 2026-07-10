fe90b142d75a5d5923b51e7f6bf49684466f79db
CuSO4-5-H2O <1983970426@qq.com>
2026-07-10 20:34:51 +0800

    feat: add Greece and Italy itinerary seed for room 1075424A

---
+++ seed-room-1075424A.js
@@ -0,0 +1,226 @@
+"use strict";
+
+module.exports = {
+  "roomId": "1075424A",
+  "list": {
+    "id": "bc014b70-dd6d-571a-a80e-1743178e493c",
+    "name": "希腊+意大利20天行程（2026.8.5）",
+    "trip": {
+      "version": 1,
+      "tripTitle": "希腊+意大利20天行程（2026.8.5）",
+      "startDate": "2026-08-05",
+      "originCity": "上海",
+      "selectedDayId": "60eeab13-047a-521e-bf2f-fe5cbd13d519",
+      "updatedAt": 1783690000000,
+      "days": [
+        {
+          "id": "60eeab13-047a-521e-bf2f-fe5cbd13d519",
+          "location": "雅典",
+          "stay": "雅典",
+          "transport": {"type": "hotel", "from": "", "to": "", "depart": "", "arrive": ""},
+          "activities": [
+            {"id": "fa773755-3813-58fa-9f91-ebeb7ce677d6", "time": "07:45", "title": "到达雅典，酒店放行李", "place": "雅典", "note": "", "done": false},
+            {"id": "a2a48491-d339-5890-9d0d-4eb069c38af3", "time": "12:00", "title": "雅典卫城", "place": "帕特农神庙、厄瑞克忒翁神庙、酒神剧场", "note": "", "done": false},
+            {"id": "4e6676ef-c2a1-56be-8338-695e4244e6f8", "time": "20:00", "title": "山顶看夜景", "place": "Filopappou Hill / Lycabettus Hill", "note": "", "done": false}
+          ]
+        },
+        {
+          "id": "f9057b05-e55d-5b33-85ee-c4808f546957",
+          "location": "夜船",
+          "stay": "夜船",
+          "transport": {"type": "boat", "from": "Piraeus", "to": "Heraklion", "depart": "21:00", "arrive": "", "},
+          "activities": [
+            {"id": "3e23bf54-ac86-5231-bf11-b287aaf231d6", "time": "09:30", "title": "国家考古博物馆", "place": "雅典", "note": "", "done": false},
+            {"id": "5e925fc9-4dc1-50d8-9bc4-baa7249b8185", "time": "12:30", "title": "市中心简餐", "place": "Exarchia或市中心", "note": "", "done": false},
+            {"id": "5ca19990-9c4c-52e5-80e4-6c0f7f3ae110", "time": "15:00", "title": "自由活动", "place": "雅典", "note": "", "done": false},
+            {"id": "f9e182b8-0b66-553f-b092-d0ddbcf9a6b6", "time": "21:00", "title": "前往伊拉克利翁", "place": "Piraeus → Heraklion", "note": "夜船或航班", "done": false}
+          ]
+        },
+        {
+          "id": "6fbb3f0f-7784-5382-ad68-1e4b37769270",
+          "location": "Heraklion",
+          "stay": "Heraklion",
+          "transport": {"type": "car", "from": "", "to": "", "depart": "", "arrive": ""},
+          "activities": [
+            {"id": "5b5e28bc-a61a-5d69-b096-cc32ea377e00", "time": "10:00", "title": "办理入住", "place": "Heraklion", "note": "", "done": false},
+            {"id": "2dbd1464-c268-56f9-92e0-8df5268abe44", "time": "12:00", "title": "米洛斯王宫", "place": "克里特", "note": "", "done": false},
+            {"id": "210bc68e-676b-52e3-b60c-80b4c80a05a8", "time": "18:00", "title": "自由活动", "place": "Heraklion", "note": "", "done": false}
+          ]
+        },
+        {
+          "id": "98378847-b0f5-5451-a42b-4206270105de",
+          "location": "雷西姆农",
+          "stay": "雷西姆农",
+          "transport": {"type": "car", "from": "", "to": "", "depart": "", "arrive": ""},
+          "activities": [
+            {"id": "3732fb05-7c23-5730-a562-b450dd618a6c", "time": "09:00", "title": "伊拉克利翁考古博物馆、威尼斯港及Koules Fortress外观", "place": "Heraklion", "note": "", "done": false},
+            {"id": "bfa2d5ac-0929-5741-a850-7f6eda09e0d6", "time": "12:30", "title": "市区午餐", "place": "Heraklion", "note": "", "done": false},
+            {"id": "d7b87d5a-08be-5917-8e1b-d48c40edbe17", "time": "15:00", "title": "粉红沙滩", "place": "克里特", "note": "", "done": false}
+          ]
+        },
+        {
+          "id": "f23b684c-072b-53b1-9a75-76347ec44859",
+          "location": "圣托里尼",
+          "stay": "圣托里尼",
+          "transport": {"type": "boat", "from": "克里特", "to": "圣托里尼", "depart": "", "arrive": ""},
+          "activities": [
+            {"id": "67659eae-0cf2-5146-9442-03462e5a9e9e", "time": "09:00", "title": "前往圣托里尼", "place": "克里特 → 圣托里尼", "note": "", "done": false}
+          ]
+        },
+        {
+          "id": "b5c817c2-72b6-5a60-9839-8a66057c9c3e",
+          "location": "圣托里尼",
+          "stay": "圣托里尼",
+          "transport": {"type": "boat", "from": "", "to": "", "depart": "", "arrive": ""},
+          "activities": [
+            {"id": "97bc7998-2c5d-5533-a51a-af1b6576ed9b", "time": "09:00", "title": "跳岛", "place": "圣托里尼", "note": "", "done": false}
+          ]
+        },
+        {
+          "id": "26d48c81-b8d8-5a6a-ae8d-831867504944",
+          "location": "西西里",
+          "stay": "西西里",
+          "transport": {"type": "plane", "from": "圣托里尼", "to": "西西里", "depart": "", "arrive": ""},
+          "activities": [
+            {"id": "f356934d-7781-5acd-b10d-2fa90220cd2c", "time": "09:00", "title": "前往机场", "place": "圣托里尼机场", "note": "", "done": false}
+          ]
+        },
+        {
+          "id": "c91e47a0-aee3-5ad1-afb4-dbb5f2ca1704",
+          "location": "Palermo",
+          "stay": "Palermo",
+          "transport": {"type": "boat", "from": "Palermo", "to": "Naples", "depart": "21:00", "arrive": ""},
+          "activities": [
+            {"id": "e5a310f0-ecae-53a2-aacd-45c39ad5aef0", "time": "09:00", "title": "Monreale大教堂或Palermo深度游", "place": "Palermo / Monreale", "note": "", "done": false},
+            {"id": "4117ea29-296f-5c16-abdc-6a7dd011c5d1", "time": "15:00", "title": "Cefalù海边小城", "place": "海滩、老城、主教座堂外观", "note": "", "done": false},
+            {"id": "b17a3dae-b649-5df0-9f78-d6defc2c058f", "time": "21:00", "title": "可选夜船前往那不勒斯", "place": "Palermo → Naples", "note": "也可以夜船去那不勒斯", "done": false}
+          ]
+        },
+        {
+          "id": "206e168a-8418-5486-a1b2-7bf84a02ca19",
+          "location": "那不勒斯",
+          "stay": "那不勒斯",
+          "transport": {"type": "train", "from": "", "to": "", "depart": "", "arrive": ""},
+          "activities": []
+        },
+        {
+          "id": "25af2563-98a5-51ca-b410-a52c33c16c76",
+          "location": "那不勒斯",
+          "stay": "那不勒斯",
+          "transport": {"type": "train", "from": "那不勒斯", "to": "庞贝", "depart": "09:00", "arrive": ""},
+          "activities": [
+            {"id": "f4d0689a-448a-5471-b5c0-46b475417815", "time": "09:00", "title": "前往庞贝", "place": "那不勒斯 → 庞贝", "note": "", "done": false},
+            {"id": "1733d283-bf11-51e0-9e73-900e385b9099", "time": "19:00", "title": "返回那不勒斯或住庞贝", "place": "庞贝", "note": "", "done": false}
+          ]
+        },
+        {
+          "id": "ab137367-c5b3-5ec2-9d6f-3fa5f3678a0e",
+          "location": "罗马",
+          "stay": "罗马",
+          "transport": {"type": "train", "from": "那不勒斯", "to": "罗马", "depart": "09:00", "arrive": ""},
+          "activities": [
+            {"id": "45b329e8-baf1-5696-9041-c83533b006bd", "time": "09:00", "title": "前往罗马", "place": "那不勒斯 → 罗马", "note": "", "done": false}
+          ]
+        },
+        {
+          "id": "3804aa95-c937-59f8-b2cf-a6d0b041b480",
+          "location": "罗马",
+          "stay": "罗马",
+          "transport": {"type": "train", "from": "", "to": "", "depart": "", "arrive": ""},
+          "activities": [
+            {"id": "b9907700-6959-5545-be35-83487f1ee91a", "time": "09:00", "title": "梵蒂冈博物馆和西斯廷礼拜堂", "place": "梵蒂冈", "note": "务必官网预约", "done": false},
+            {"id": "00841df3-9338-5c90-9398-507a770f7f03", "time": "12:30", "title": "Borgo或Prati区午餐", "place": "罗马", "note": "", "done": false},
+            {"id": "0d437275-1808-5101-8c3d-f47efaa3a855", "time": "15:00", "title": "圣彼得大教堂、圣彼得广场", "place": "可加圣天使堡外观", "note": "", "done": false},
+            {"id": "bfe06895-8ff7-5e09-9e88-45e97d35681c", "time": "19:00", "title": "罗马市区自由活动", "place": "罗马", "note": "梵蒂冈博物馆务必官网预约", "done": false}
+          ]
+        },
+        {
+          "id": "5ae8a63b-ea53-5232-a7b2-09f915f09787",
+          "location": "罗马",
+          "stay": "罗马",
+          "transport": {"type": "train", "from": "", "to": "", "depart": "", "arrive": ""},
+          "activities": [
+            {"id": "39f6b767-6555-5a83-ae10-5ee83116eec3", "time": "09:00", "title": "斗兽场预约早场", "place": "罗马", "note": "务必提前预约具体时段", "done": false},
+            {"id": "0ac6b5f4-f677-5b92-8f18-f2341dff6028", "time": "12:30", "title": "Monti区午餐", "place": "罗马", "note": "", "done": false},
+            {"id": "e17d5a75-4574-516d-8575-38103574c537", "time": "15:00", "title": "古罗马广场、帕拉蒂尼山、威尼斯广场、卡比托利欧山", "place": "罗马", "note": "", "done": false},
+            {"id": "b4079d17-9599-5c1c-b467-4a21cdf26d0f", "time": "19:00", "title": "Trastevere晚餐", "place": "罗马", "note": "斗兽场务必提前预约具体时段", "done": false}
+          ]
+        },
+        {
+          "id": "e53b1124-3954-55c3-9e14-ab366490748e",
+          "location": "佛罗伦萨",
+          "stay": "佛罗伦萨",
+          "transport": {"type": "train", "from": "罗马", "to": "佛罗伦萨", "depart": "", "arrive": ""},
+          "activities": [
+            {"id": "af9c02ed-11cd-5171-8b19-199e8b188c86", "time": "12:00", "title": "蒙特普尔恰诺", "place": "托斯卡纳", "note": "", "done": false},
+            {"id": "cab18f3b-5fb0-5e00-8726-79a065772766", "time": "15:00", "title": "前往佛罗伦萨", "place": "佛罗伦萨", "note": "", "done": false}
+          ]
+        },
+        {
+          "id": "0d940275-befc-5497-8594-4c0fb3602b3d",
+          "location": "佛罗伦萨",
+          "stay": "佛罗伦萨",
+          "transport": {"type": "train", "from": "", "to": "", "depart": "", "arrive": ""},
+          "activities": [
+            {"id": "d23d3da7-295f-51e7-9aad-176430d3bb4f", "time": "09:00", "title": "乌菲兹美术馆", "place": "佛罗伦萨", "note": "提前预约", "done": false},
+            {"id": "d67a53ec-7fc4-5db3-9b7b-6ad1c3953595", "time": "12:30", "title": "Mercato Centrale或老城简餐", "place": "佛罗伦萨", "note": "", "done": false},
+            {"id": "c085173b-98e0-5dd3-99d1-1d83a9a1bba4", "time": "15:00", "title": "圣母百花大教堂外观、领主广场、老桥", "place": "佛罗伦萨", "note": "", "done": false},
+            {"id": "1011499a-9eb0-512a-a020-1e5321a93b69", "time": "19:00", "title": "米开朗琪罗广场日落", "place": "佛罗伦萨", "note": "乌菲兹提前预约", "done": false}
+          ]
+        },
+        {
+          "id": "93c0cb4b-ee81-5121-8559-238463b41cf3",
+          "location": "佛罗伦萨",
+          "stay": "佛罗伦萨",
+          "transport": {"type": "train", "from": "佛罗伦萨", "to": "比萨", "depart": "15:00", "arrive": ""},
+          "activities": [
+            {"id": "25b108d8-604f-51d3-9374-e50f33bcf588", "time": "09:00", "title": "学院美术馆看《大卫》或圣十字圣殿/圣洛伦佐市场", "place": "佛罗伦萨", "note": "", "done": false},
+            {"id": "bf82a4dc-a427-5a51-92f2-4a6e86f88bc2", "time": "12:30", "title": "佛罗伦萨午餐", "place": "佛罗伦萨", "note": "", "done": false},
+            {"id": "b64e16bd-f0de-5e87-b4dc-b2eb74349403", "time": "15:00", "title": "火车往返比萨", "place": "奇迹广场、斜塔外观、主教座堂", "note": "", "done": false},
+            {"id": "164ae8c6-1008-5fa4-89d7-284911e53046", "time": "20:00", "title": "返回佛罗伦萨休息", "place": "佛罗伦萨", "note": "比萨半日足够；登塔非必需", "done": false}
+          ]
+        },
+        {
+          "id": "8bfef2c8-a571-5396-b6e5-69d91404b379",
+          "location": "威尼斯",
+          "stay": "威尼斯",
+          "transport": {"type": "train", "from": "佛罗伦萨", "to": "威尼斯", "depart": "", "arrive": ""},
+          "activities": []
+        },
+        {
+          "id": "c42f10c6-8a44-5b1d-b438-10aa3c5ff195",
+          "location": "米兰",
+          "stay": "米兰",
+          "transport": {"type": "train", "from": "威尼斯", "to": "米兰", "depart": "", "arrive": ""},
+          "activities": []
+        },
+        {
+          "id": "e0296407-f0fd-5023-81cd-81ce9f33f314",
+          "location": "热那亚",
+          "stay": "热那亚",
+          "transport": {"type": "plane", "from": "Palermo/Catania", "to": "Genoa", "depart": "09:00", "arrive": ""},
+          "activities": [
+            {"id": "501fa1bb-e5d1-5789-b34a-53eedff112e6", "time": "09:00", "title": "飞往热那亚", "place": "Palermo/Catania → Genoa；若票价高则飞Milan/Pisa后转火车", "note": "", "done": false},
+            {"id": "4bb35888-76ba-56af-9580-ca93e8b3905a", "time": "12:30", "title": "抵达后寄存或入住", "place": "热那亚", "note": "", "done": false},
+            {"id": "89ba26f3-2e1e-531c-88dd-f9cf6b5eadcf", "time": "15:00", "title": "热那亚老港、Via Garibaldi及老城街巷", "place": "热那亚", "note": "", "done": false},
+            {"id": "921ebee2-0c91-5d6c-a2fc-f8f710fd75a8", "time": "19:00", "title": "品尝focaccia", "place": "热那亚", "note": "西西里→北意通勤+热那亚半日", "done": false}
+          ]
+        },
+        {
+          "id": "f3a51264-3505-5ed6-9609-0856e853645c",
+          "location": "飞机上",
+          "stay": "飞机上",
+          "transport": {"type": "plane", "from": "欧洲", "to": "上海", "depart": "", "arrive": ""},
+          "activities": [
+            {"id": "a287fd52-745f-5fc4-90a7-01924d288f8f", "time": "08:00", "title": "前往机场", "place": "返程城市", "note": "", "done": false},
+            {"id": "f8db67cb-1725-57f0-ac08-1b9197828c98", "time": "11:00", "title": "机场退税、值机、安检", "place": "机场", "note": "", "done": false},
+            {"id": "8b39045b-4bcc-5420-b15a-5cbd7588fc18", "time": "14:00", "title": "飞回上海", "place": "返程航班", "note": "", "done": false},
+            {"id": "01c893c1-777b-57b5-90e0-5be11d7aeedd", "time": "20:00", "title": "飞行途中", "place": "飞机上", "note": "若从米兰走，建议前一晚已到米兰，不要当天从威尼斯赶机", "done": false}
+          ]
+        }
+      ]
+    },
+    "createdAt": 1783690000000,
+    "updatedAt": 1783690000000
+  }
+};
