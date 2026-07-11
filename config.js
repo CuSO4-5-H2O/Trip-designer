4753c9569140cda1b8aadef9ed7649849ab9a0b1
CuSO4-5-H2O <1983970426@qq.com>
2026-07-12 01:15:18 +0800

    perf: load contextual editor performance styles

--- config.js
+++ config.js
@@ -5,18 +5,22 @@ window.TRIP_PLANNER_CONFIG = {
 };
 
 (() => {
-  if (!document.querySelector('link[data-activity-tags="true"]')) {
-    const link = document.createElement("link");
-    link.rel = "stylesheet";
-    link.href = "./activity-tags.css?v=tags-20260712a";
-    link.dataset.activityTags = "true";
-    document.head.append(link);
-  }
+  loadStyle("context-editor-performance", "./context-editor-performance.css?v=context-perf-20260712b");
+  loadStyle("activity-tags", "./activity-tags.css?v=tags-20260712a");
 
   if (!document.querySelector('script[data-activity-tags="true"]')) {
     const script = document.createElement("script");
     script.src = "./activity-tags.js?v=tags-20260712a";
     script.dataset.activityTags = "true";
     document.body.append(script);
+  }
+
+  function loadStyle(name, href) {
+    if (document.querySelector(`link[data-trip-style="${name}"]`)) return;
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = href;
+    link.dataset.tripStyle = name;
+    document.head.append(link);
   }
 })();
