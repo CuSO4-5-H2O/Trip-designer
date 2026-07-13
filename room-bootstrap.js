710481afea7b730a6d7c5b8b76d950268bbaa677
CuSO4-5-H2O <1983970426@qq.com>
2026-07-14 01:33:29 +0800

    Bump inline editor template build

--- room-bootstrap.js
+++ room-bootstrap.js
@@ -2,7 +2,7 @@
   "use strict";
 
   const APP_BUILD_VERSION = "render-20260713y";
-  const INLINE_EDITOR_VERSION = "render-20260713aa";
+  const INLINE_EDITOR_VERSION = "render-20260714h";
   const LAST_ROOM_KEY = "tripdesigner:last-room";
   const params = new URLSearchParams(location.search);
   const requestedRoomId = normalizeRoom(params.get("room"));
