95883b7cae345262a5ddd18ffa81e62b0658181d
CuSO4-5-H2O <1983970426@qq.com>
2026-07-14 10:01:07 +0800

    Bump app bundle for render stability fixes

--- room-bootstrap.js
+++ room-bootstrap.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const APP_BUILD_VERSION = "render-20260713y";
+  const APP_BUILD_VERSION = "render-20260714j";
   const INLINE_EDITOR_VERSION = "render-20260714h";
   const LAST_ROOM_KEY = "tripdesigner:last-room";
   const params = new URLSearchParams(location.search);
